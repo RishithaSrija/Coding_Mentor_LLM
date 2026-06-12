281c86d84dc463459a0ce435659528ef4e8dc010
Rishitha Srija <rishitha@example.com>
2026-06-12 09:51:05 +0530

    Added_openrouter_api_key

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from transformers import AutoTokenizer, AutoModelForCausalLM
+# from transformers import AutoTokenizer, AutoModelForCausalLM
 from fastapi.middleware.cors import CORSMiddleware
-import torch
+# import torch
+import requests
+import os
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
  
 app = FastAPI()
 app.add_middleware(
@@ -12,43 +16,44 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-model_path = "./tinyllama-coding-model"
+# model_path = "./tinyllama-coding-model"
  
 print("Loading model...")
  
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForCausalLM.from_pretrained(model_path)
+# tokenizer = AutoTokenizer.from_pretrained(model_path)
+# model = AutoModelForCausalLM.from_pretrained(model_path)
  
 class Question(BaseModel):
     prompt: str
  
 @app.post("/ask")
 def ask_ai(data: Question):
-    text = f"""
-You are a professional coding assistant.
-Give clear and correct coding answers.
- 
-Instruction: {data.prompt}
- 
-Answer:
-"""
- 
-    inputs = tokenizer(text, return_tensors="pt")
- 
-    with torch.no_grad():
-        outputs = model.generate(
-    **inputs,
-    max_new_tokens=350,
-    temperature=0.2,
-    top_p=0.9,
-    do_sample=True,
-    repetition_penalty=1.15,
-    no_repeat_ngram_size=3,
-    pad_token_id=tokenizer.eos_token_id
-)
- 
-    result = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    answer = result.replace(text, "").strip()
- 
-    return {"response": answer}
+
+    response = requests.post(
+        "https://openrouter.ai/api/v1/chat/completions",
+        headers={
+            "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+            "Content-Type": "application/json",
+        },
+        json={
+            "model": "deepseek/deepseek-r1-0528-qwen3-8b:free",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": "You are a professional coding assistant. Give clear and correct coding answers."
+                },
+                {
+                    "role": "user",
+                    "content": data.prompt
+                }
+            ],
+            "temperature": 0.2,
+            "max_tokens": 350
+        }
+    )
+
+    result = response.json()
+    return {
+        "response": result["choices"][0]["message"]["content"]
+    }
  
